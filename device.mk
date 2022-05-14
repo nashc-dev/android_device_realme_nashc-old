1e4bd05cd8e87e27c15d23a38c35b236a43cc5fb
Bowgo Tsai <bowgotsai@google.com>
2022-05-14 09:43:58 +0100

    nashc: device: Adding developer GSI keys Bug: 144399552 Test: tree-hugger

--- device.mk
+++ device.mk
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
+
 # Shipping Level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
