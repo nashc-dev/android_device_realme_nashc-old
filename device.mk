a5cf68881df8a2e6666bad805e1ce3a567f8c096
bengris32 <bengris32@protonmail.ch>
2022-06-19 20:04:54 +0100

    Revert "nashc: Enable updateable APEX"
    This reverts commit 374fae7940aa4e3502df83ddde98edb8590f8c47.

--- device.mk
+++ device.mk
@@ -17,9 +17,6 @@
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Shipping Level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
