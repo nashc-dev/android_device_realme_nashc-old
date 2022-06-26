f0de8328017b38f06e3e48ba5de37fd15576aef4
TheMalachite <eliasgheeraert@gmail.com>
2022-06-26 18:07:37 +0100

    nashc: sepolicy: Address oplus charger service denials

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -178,6 +178,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
 # SELinux
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # VNDK
 BOARD_VNDK_VERSION := current
