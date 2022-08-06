febb25d15dc10e61f260f703c7ada9577e670891
bengris32 <bengris32@protonmail.ch>
2022-08-06 20:49:58 +0100

    nashc: vibrator: Add basic Vibrator AIDL service * Very barebones implementation that just uses LED vibrator   trigger.
    * Based on the QTI vibrator service, with unrelated haptic
      cruft removed.
    
    * Also drop the prebuilt MTK vibrator service.
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -397,6 +397,10 @@ PRODUCT_PACKAGES += \
     android.hardware.usb.gadget@1.0.vendor \
     android.hardware.usb.gadget@1.1.vendor
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator-service.nashc
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.1.vendor \
