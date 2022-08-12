029c12508c10d771175b21107b6a811514f5e52f
bengris32 <bengris32@protonmail.ch>
2022-08-12 23:08:24 +0100

    nashc: Switch to OSS Power AIDL * Make libpowerhal.so a module so we can link against it. * Also drop the prebuilt implementation and vintf fragment.
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -332,6 +332,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power-service-mediatek \
     android.hardware.power@1.0.vendor \
     android.hardware.power@1.1.vendor \
     android.hardware.power@1.2.vendor
