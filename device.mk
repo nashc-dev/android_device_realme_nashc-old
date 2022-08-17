d7b49c0dbaec70c4d3c9417f7beea23554139e6b
bengris32 <bengris32@protonmail.ch>
2022-08-17 21:34:49 +0100

    nashc: Link mtk_agpsd against V32 libcrypto * cannot locate symbol "lh_free" referenced by "/vendor/bin/mtk_agpsd"...
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -401,7 +401,8 @@ PRODUCT_PACKAGES += \
 # VNDK
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v32/arm/arch-arm-armv7-a-neon/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib/libutils-v32.so \
-    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v32.so
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v32.so \
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-core/libcrypto.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcrypto-v32.so
 
 # WiFi
 PRODUCT_PACKAGES += \
--- extract-files.sh
+++ extract-files.sh
@@ -47,6 +47,9 @@ function blob_fixup {
         vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so)
            "$PATCHELF" --replace-needed "android.hardware.gnss-V1-ndk_platform.so" "android.hardware.gnss-V1-ndk.so" "${2}"
             ;;
+        vendor/bin/mtk_agpsd)
+           "$PATCHELF" --replace-needed "libcrypto.so" "libcrypto-v32.so" "${2}"
+            ;;
         vendor/bin/hw/android.hardware.media.c2@1.2-mediatek)
             ;&
         vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b)
