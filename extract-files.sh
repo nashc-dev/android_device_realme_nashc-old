036074a626f2f1a384963d2de0294ca5209f7e67
Jiyong Park <jiyong@google.com>
2022-08-17 18:39:55 +0100

    nashc: mtkpower: Remove ndk_platform backend. Use the ndk backend. The ndk_platform backend will soon be deprecated because the ndk backend can serve the same purpose. This is to eliminate the confusion about having two variants (ndk and ndk_platform) for the same ndk backend.
    Bug: 161456198
    Test: m

--- extract-files.sh
+++ extract-files.sh
@@ -39,6 +39,9 @@ function blob_fixup {
         vendor/lib64/hw/vendor.mediatek.hardware.pq@2.13-impl.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             ;;
+        vendor/bin/hw/vendor.mediatek.hardware.mtkpower@1.0-service)
+            "$PATCHELF" --replace-needed "android.hardware.power-V2-ndk_platform.so" "android.hardware.power-V2-ndk.so" "${2}"
+            ;;
     esac
 }
 
