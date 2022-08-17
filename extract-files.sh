5550d3dde4c331c7d07d1b068a581fa5c6e841ee
Jiyong Park <jiyong@google.com>
2022-08-17 18:45:15 +0100

    nashc: gnss: Remove ndk_platform backend. Use the ndk backend. The ndk_platform backend will soon be deprecated because the ndk backend can serve the same purpose. This is to eliminate the confusion about having two variants (ndk and ndk_platform) for the same ndk backend.
    Bug: 161456198
    Test: m

--- extract-files.sh
+++ extract-files.sh
@@ -42,6 +42,11 @@ function blob_fixup {
         vendor/bin/hw/vendor.mediatek.hardware.mtkpower@1.0-service)
             "$PATCHELF" --replace-needed "android.hardware.power-V2-ndk_platform.so" "android.hardware.power-V2-ndk.so" "${2}"
             ;;
+        vendor/bin/hw/android.hardware.gnss-service.mediatek)
+            ;&
+        vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so)
+           "$PATCHELF" --replace-needed "android.hardware.gnss-V1-ndk_platform.so" "android.hardware.gnss-V1-ndk.so" "${2}"
+            ;;
     esac
 }
 
