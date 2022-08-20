453b8e93c7fdfea5a1bec71c0c29c035c0bf3313
bengris32 <bengris32@protonmail.ch>
2022-08-20 11:19:24 +0100

    [DNM] nashc: wifi: Switch back to prebuilt hostapd service
    This reverts commit 9c4b909361faf03933653b194f697dee6c9698dd.

--- device.mk
+++ device.mk
@@ -423,8 +423,7 @@ PRODUCT_PACKAGES += \
     android.hardware.tetheroffload.config@1.0.vendor \
     android.hardware.tetheroffload.control@1.0.vendor \
     android.hardware.tetheroffload.control@1.1.vendor \
-    android.hardware.wifi@1.0-service-lazy.nashc \
-    hostapd
+    android.hardware.wifi@1.0-service-lazy.nashc
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
