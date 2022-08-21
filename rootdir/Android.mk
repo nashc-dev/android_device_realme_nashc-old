eb1a77653276ec181002230c0d158009fea17480
bengris32 <bengris32@protonmail.ch>
2022-08-21 16:33:43 +0100

    nashc: rootdir: Implement CABC support for RealmeParts
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -18,6 +18,14 @@ LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE	   := cabc.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/cabc.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.ago.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
