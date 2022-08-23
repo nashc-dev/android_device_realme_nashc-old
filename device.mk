2af92c689c8e613c0afe97ee77cf536a05f15dda
Josh Wu <joshwu@google.com>
2022-08-23 11:36:11 +0100

    nashc: Switch BtAudio to AIDL Test: m Bug: 203490261

--- device.mk
+++ device.mk
@@ -162,8 +162,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth@1.1.vendor \
-    android.hardware.bluetooth.audio@2.0-impl \
-    android.hardware.bluetooth.audio@2.1-impl
+    android.hardware.bluetooth.audio-impl
 
 # Biometrics
 PRODUCT_PACKAGES += \
